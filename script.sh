73f50a06d8548fc2b9ad44dfc8465c7a851a4fda
saivinay98 <68029647+saivinay98@users.noreply.github.com>
2020-08-20 21:54:01 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ sudo apt-get update
 sudo apt-get -y install ansible
 sudo apt-get -y install git
 git clone https://github.com/saivinay98/ansible-play.git
-cd ansible-play; sudo ansible-playbook main.yml
+cd ansible-play; sudo ansible-playbook main.yml -vv
